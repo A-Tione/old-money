30265464609c5e239e1979ec2999c895590da7eb
atione <heyanggo@gmail.com>
2024-11-18 17:17:22 +0800

    feat: add welcome page

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,3 +1,8 @@
+import { First } from '../components/welcome/first'
+import { Forth } from '../components/welcome/forth'
+import { Second } from '../components/welcome/second'
+import { Third } from '../components/welcome/third'
+import { Welcome } from '../views/welcome'
 import {Bar} from '../views/Bar'
 import { Foo } from '../views/Foo'
 
@@ -6,4 +11,14 @@ import { Foo } from '../views/Foo'
 export const routes = [
   {path: '/', component: Foo},
   {path: '/about', component: Bar},
+  {
+    path: '/welcome', 
+    component: Welcome,
+    children: [
+      {path: '1', component: First},
+      {path: '2', component: Second},
+      {path: '3', component: Third},
+      {path: '4', component: Forth},
+    ]
+  }
 ]
