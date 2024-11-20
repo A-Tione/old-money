1cdfb1b2e94513aea057119bd53c97ba7e0d4274
atione <heyanggo@gmail.com>
2024-11-20 15:57:49 +0800

    feat: optimsize welcomeLayout

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,22 +1,18 @@
-import { defineComponent } from 'vue';
+import { FunctionalComponent } from 'vue';
 import pig from "../../assets//icons/pig.svg"
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import { RouterLink } from 'vue-router';
 import {WelcomeLayout} from './WelcomeLayout'
-export const First = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{ 
-          icon: () => <img class={s.icon} src={pig}/>,
-          title: () => <h2>会挣钱<br/>还要会省钱</h2>,
-          buttons: () => <>
-            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-            <RouterLink to="/welcome/2">下一页</RouterLink>
-            <RouterLink to="/start">跳过</RouterLink>
-          </>
-        }}
-      </WelcomeLayout>
-    )
-  }
-})
+export const First: FunctionalComponent = () => {
+  return <WelcomeLayout>
+      {{ 
+        icon: () => <img class={s.icon} src={pig}/>,
+        title: () => <h2>会挣钱<br/>还要会省钱</h2>,
+        buttons: () => <>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </>
+      }}
+    </WelcomeLayout>
+}
