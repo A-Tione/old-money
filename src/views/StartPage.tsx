1c2b2e228d00ea9b82551e9bbbfbfc69e30c4df7
atione <heyanggo@gmail.com>
2024-12-02 17:27:03 +0800

    feat: add overlay component

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,30 +1,36 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import s from './StartPage.module.scss'
 import { Button } from "../shared/Button";
 import { FloatButton } from "../shared/FloatButton";
 import { Center } from "../shared/Center";
 import { Icon } from "../shared/Icon";
 import { Navbar } from "../shared/Navbar";
+import { Overlay } from "../shared/Overlay";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const onClick = () => {
-      console.log('hi'); 
+    const refOverlayVisble = ref(false)
+    const onClickMenu = () => {
+      refOverlayVisble.value = !refOverlayVisble.value
+      console.log(refOverlayVisble.value);
     }
 
     return () => (
       <div>
         <Navbar>{{
-          default: '山竹记账',
-          icon: <Icon name='menu' class={s.navIcon}/>
+          default: () => '山竹记账',
+          icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>
         }}</Navbar>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig}></Icon>
         </Center>
         <div class={s.button_warpper}>
-          <Button class={s.button} onClick={onClick}>开始记账</Button>
+          <Button class={s.button}>开始记账</Button>
         </div>
         <FloatButton iconName="add"/>
+        { refOverlayVisble.value && 
+          <Overlay onClose={() => refOverlayVisble.value = false} />
+        }
       </div>
     )
   }
