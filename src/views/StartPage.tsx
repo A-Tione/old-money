631e60defd3eba33fe768fd442c1ae10d796f819
atione <heyanggo@gmail.com>
2024-12-03 11:26:37 +0800

    feat: add mainLayout component

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -7,6 +7,7 @@ import { Icon } from "../shared/Icon";
 import { Navbar } from "../shared/Navbar";
 import { Overlay } from "../shared/Overlay";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -16,24 +17,25 @@ export const StartPage = defineComponent({
     }
 
     return () => (
-      <div>
-        <Navbar>{{
-          default: () => '山竹记账',
-          icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>
-        }}</Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig}></Icon>
-        </Center>
-        <RouterLink to="/items/create">
-          <Button class={s.button}>开始记账</Button>
-        </RouterLink>
-        <RouterLink to="/items/create">
-          <FloatButton iconName="add"/>
-        </RouterLink>
-        { refOverlayVisble.value && 
-          <Overlay onClose={() => refOverlayVisble.value = false} />
-        }
-      </div>
+      <MainLayout>{{
+        title: () => '山竹记账',
+        icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>,
+        main: () => <>
+          <Center class={s.pig_wrapper}>
+            <Icon name="pig" class={s.pig}></Icon>
+          </Center>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
+          <RouterLink to="/items/create">
+            <FloatButton iconName="add"/>
+          </RouterLink>
+          { refOverlayVisble.value && 
+            <Overlay onClose={() => refOverlayVisble.value = false} />
+          }
+        </>
+      }}
+      </MainLayout>
     )
   }
 })
